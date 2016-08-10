70779b822da1a42258bbce183992224748371a12
Bill Langley <brainstormwilly@gmail.com>
2016-08-10 08:34:43 -0700

    Checkpoint Create: Completed

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,6 +1,7 @@
 class Api::ListsController < ApiController
 
   before_action :authenticated?
+  before_action :authorize_user, only: [:create]
 
   def index
     lists = List.all
@@ -21,5 +22,13 @@ class Api::ListsController < ApiController
   def list_params
     params.require(:list).permit(:title, :description)
   end
+
+
+  def authorize_user
+    unless @current_user.id.to_s == params['user_id']
+      render json: {error: "Authorization Failure", status: 400}, status: 400
+    end
+  end
+
 
 end
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -13,4 +13,5 @@ class ApiController < ApplicationController
     }
   end
 
+  
 end
